d3c6e79d925fa7a19e85c2e2ad6549658021cda6
rbeck4 <rbeck4@uw.edu>
2022-11-17 20:01:53 -0800

    is a screne, sends a start_game signal... doesn't do anything with it and so just sits there but is a thing nonetheless

---
+++ Opening.gd
@@ -0,0 +1,35 @@
+extends CanvasLayer
+
+signal start_game
+
+func show_message(text):
+	$MessageLabel.text = text
+	$MessageLabel.show()
+	$MessageTimer.start()
+	
+func show_game_over():
+	show_message("Game Over")
+	await get_tree().create_timer(1000).timeout
+	$MessageLabel.text = "A cliche way"
+	$MessageLabel.show()
+	await get_tree().create_timer(1000).timeout
+	$StartButton.show()
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
+	
+func _on_StartButton_pressed():
+	$StartrButton.hide()
+	emit_signal("start_game")
+	
+func _on_MessageTimer_timeout():
+	$MessageLabel.hide()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
