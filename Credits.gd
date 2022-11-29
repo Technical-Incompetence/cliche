cfa1df8c4af093ba81249687e866ada1bed7ad2c
Chris Yealy <christopherjyealy@gmail.com>
2022-11-29 17:21:29 -0500

    Added credits and tweaked UI

---
+++ Credits.gd
@@ -0,0 +1,5 @@
+extends Control
+
+func _on_button_pressed():
+	print("PLZ")
+	get_tree().change_scene_to_file("res://Opening.tscn")
--- Opening.gd
+++ Opening.gd
@@ -25,15 +25,9 @@ func _on_StartButton_pressed():
 func _on_MessageTimer_timeout():
 	$MessageLabel.hide()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://GameView.tscn")
+
+
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file("res://Credits.tscn")
